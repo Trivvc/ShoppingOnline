b769181747aee41460c17165c46752038a0901a8
Trivvc <93276759+Trivvc@users.noreply.github.com>
2024-07-04 14:10:57 +0700

    upload server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants={
+  DB_SERVER:'db01.nnavhxy.mongodb.net',
+  DB_USER:'ntcc20122003',
+  DB_PASS:'6eRcnxKESReC7IZK',
+  DB_DATABASE:'ShoppingOnline',
+  JWT_SECRET:'KHOA_BI_MAT',
+  JWT_EXPIRES:'481571982475128374823742834',
+  EMAIL_USER:'ntcc20122003@gmail.com',
+  EMAIL_PASS:'lvrxrpijdeaygnvk',
+
+};
+module.exports = MyConstants;
+
+
