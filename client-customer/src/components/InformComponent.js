4bc4d58d54f44aea78aa51722b859a438b1a6f2b
Trivvc <93276759+Trivvc@users.noreply.github.com>
2024-07-20 06:54:18 +0700

    upload client-customer

--- client-customer/src/components/InformComponent.js
+++ client-customer/src/components/InformComponent.js
@@ -8,24 +8,37 @@ class Inform extends Component {
     return (
       <div className="border-bottom navbar--1">
         <div className="float-left">
-        {this.context.token === '' ?
-          <div className="active__cus">
-            <Link to='/login'>Login</Link> 
-            <Link to='/signup'>Sign-up</Link> 
-           <Link to='/active'>Active</Link></div>
-          :
-          <div>Hello <b>{this.context.customer.name}</b> | <Link to='/home' onClick={() => this.lnkLogoutClick()}>Logout</Link> | <Link to='./myprofile'>My profile</Link> | <Link to='/myorders'>My orders</Link></div>
-        }
-      </div>
+          {this.context.token === "" ? (
+            <div className="active__cus">
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign-up</Link>
+              <Link to="/active">Active</Link>
+            </div>
+          ) : (
+            <div className="active__cus">
+              Hello <b>{this.context.customer.name}</b>  |{" "}
+              <Link to="/home" onClick={() => this.lnkLogoutClick()}>
+                Logout
+              </Link>{" "}
+              <Link to="./myprofile">My profile</Link>{" "}
+              <Link to="/myorders">My orders</Link>
+            </div>
+          )}
+        </div>
         <div className="float-right myCart--right">
-        <Link to='/mycart'>My cart<p>have <b>{this.context.mycart.length}</b> items</p></Link> 
+          <Link to="/mycart">
+            My cart
+            <p>
+              have <b>{this.context.mycart.length}</b> items
+            </p>
+          </Link>
         </div>
       </div>
     );
   }
-// event-handlers
+  // event-handlers
   lnkLogoutClick() {
-    this.context.setToken('');
+    this.context.setToken("");
     this.context.setCustomer(null);
     this.context.setMycart([]);
   }
